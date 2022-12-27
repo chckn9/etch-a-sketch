db649b268ed44ba3f2618abd81c7465e1dd5dcf3
chckn9 <thomasyeoh9@gmail.com>
2022-12-27 13:09:48 -0800

    Add user prompt for dimensions + Load new canvas everytime

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
 // Generates a canvas given the grid dimensions
-function createCanvas() {
+function createCanvas(dim) {
     // TO DO: Make it so user chooses dimensions
     // *Temporarily: Create 16x16 grid
-    const square_size = 512 / 16;
+    const square_size = 512 / dim;
     const container = document.querySelector('.canvas');
-    for (let i = 0; i < 16 * 16; i++) {
+    // Remove previous squares
+    document.querySelectorAll('.square').forEach(e => e.remove());
+
+    for (let i = 0; i < dim * dim; i++) {
         const square = document.createElement('div');
         square.classList.add("square");
         square.style.height = square_size + 'px';
@@ -13,11 +16,34 @@ function createCanvas() {
     }
 }
 
-createCanvas();
-
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-    square.addEventListener('mouseover', function() {
-        square.classList.add("hovered");
+// Add event listener to squares
+function addListener() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', function() {
+            square.classList.add("hovered");
+        })
     })
-})
+}
+
+// Load initial canvas
+createCanvas(32);
+addListener();
+
+// Prompt user for dimensions when button is clicked
+function promptDimensions() {
+    let text;
+    let size = prompt("Enter width/height of canvas (max 100):", "32");
+    if (size === null || size === "") {
+        text = "user canceled the prompt";
+    }
+    else if (size > 100 || size < 1) {
+        text = "you can only pick a number from 1 - 100"
+    }
+    else {
+        createCanvas(size);
+        addListener();
+        text = "";
+    }
+    document.getElementById("output").innerHTML = text;
+}
